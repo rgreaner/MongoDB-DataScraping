5b3c2f07cd565c76501500ca16f424f3ed5e5aca
Rachel Greaner <rgreaner8@gmail.com>
2018-04-30 18:25:53 -0500

    Scraping works in console.log

--- server.js
+++ server.js
@@ -26,24 +26,41 @@ mongoose.connect("mongodb://localhost/bbcArticle");
 app.get("/scrape", function (req, res) {
     request("http://www.bbc.com/news", function (error, response, html) {
         var $ = cheerio.load(html);
+        
 
-        $(".title").each(function (i, element) {
-            var title = $(element).children("a").text();
-            var link = $(element).children("a").attr("href");
-            if (title && link) {
-                db.bbcArticle.insert({
-                    title: title,
-                    link: link
-                });
-            };
+        $("a.gs-c-promo-heading").each(function (i, element) {
+            var result = {};
+            console.log($(element).children('h3').text());
+            console.log($(element).attr("href"));
+
+            console.log("**************************")
+            // var title = $(element).children("a").text();
+            // var link = $(element).children("a").attr("href");
+            // if (title && link) {
+            //     db.bbcArticle.insert({
+            //         title: title,
+            //         link: link
+            //     });
+            // };
+            // Create a new Article using the `result` object built from scraping
+            // db.article.create(result)
+            //     .then(function (dbarticle) {
+            //         // View the added result in the console
+            //         console.log(dbarticle);
+            //     })
+            //     .catch(function (err) {
+            //         // If an error occurred, send it to the client
+            //         return res.json(err);
+            //     });
         });
         res.send("Articles Scraped");
-        console.log("hey", response)
+        // console.log("hey", response)
     });
 });
 
 
+// });
 //Listen on the Port
-app.listen(3000, function() {
-    console.log("App runnin on port 3000!");
+app.listen(3000, function () {
+    console.log("App running on port 3000!");
 });
